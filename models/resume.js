25824c22ee869c1bf4eb7adc34bdc770c15ee2b3
lhj0621 <lhj0621@gmail.com>
2020-06-21 19:58:39 +0900

    s3 세팅 / 업로드 폼 / 리스트 제작

--- models/resume.js
+++ models/resume.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 const Sequelize = require('sequelize');
-const config = require(__dirname+'/../config/config.json')['development'];
+const config = require(__dirname + '/../config/config.json')['development'];
 
 const AWS = require('aws-sdk');
-AWS.config.loadFromPath('../aws_config.json');
+AWS.config.loadFromPath(__dirname + '/../config/aws_config.json');
+
 
 const sequelize = new Sequelize({
     username: config.username,
     password: config.password,
     database: config.database,
     host: config.host,
-    dialect: 'mysql' 
+    dialect: 'mysql'
 });
 
 class resumes extends Sequelize.Model { }
 
 resumes.init({
     title: Sequelize.STRING(40),
+    img_name: Sequelize.STRING(40),
     img_url: Sequelize.STRING(100)
 }, { tableName: 'resume', timestamps: false, sequelize });
 
@@ -35,6 +37,34 @@ class resume {
         }
         catch (error) {
             console.log('country.sync Error ', error);
+        }
+    }
+
+    async resumeList() {
+        let returnval;
+        await resumes.findAll({})
+            .then(results => {
+                returnval = results;
+            })
+            .catch(error => {
+                console.error('Error :', error);
+            });
+        return returnval;
+    }
+
+    async addresume(data) {
+        try {
+            const ret = await resumes.create({
+                title: data.title,
+                img_name: data.img_name,
+                img_url: data.img_url
+            }, { logging: false });
+            const newData = ret.dataValues;
+            console.log(newData);
+            console.log('Create success');
+        }
+        catch (error) {
+            console.log('Error : ', error);
         }
     }
 }
--- routes/resume.js
+++ routes/resume.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
+const resume = require('../models/resume');
+const path = require("path");
+const multer = require("multer");
+const multerS3 = require('multer-s3');
+const AWS = require("aws-sdk");
+var s3 = new AWS.S3();
+const now = new Date();
+const itemKey = 'upload/' + now.getHours() + now.getMinutes() + now.getSeconds() + Math.floor(Math.random() * 1000);
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+let upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "idu-2020-0709",
+    key: function (req, file, cb) {
+      let extension = path.extname(file.originalname) //확장자 
+      cb(null, itemKey + extension) // 파일 저장 이름
+    },
+    acl: 'public-read',
+    mimetype: 'image/jpeg',
+    location: "/upload"
+  })
+})
+router.get('/', resumeList);
+router.get('/imgupload', imguploadform);
+router.post('/imgupload', upload.single('img'), addimg);
 
 module.exports = router;
+
+async function resumeList(req, res) {
+  const resumeList = await resume.resumeList();
+  const result = { count: resumeList.length,data: resumeList };
+  res.render('index', { result: result });
+}
+
+function imguploadform(req, res) {
+  res.render('img_upload');
+}
+
+async function addimg(req, res) {
+  const title = req.body.title;
+  const img_name = req.file.key;
+  const img_url = req.file.location;
+  const data = {
+    "title": title,
+    "img_name" : img_name,
+    "img_url" : img_url,
+  };
+  console.log(data);
+  try {
+      const result = await resume.addresume(data);
+      res.redirect('/resume/');
+  } catch (error) {
+      res.status(500).send(error.msg);
+  }
+}
+
+
